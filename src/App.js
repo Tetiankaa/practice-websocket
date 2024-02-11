625163a96228dcc1e82205370b2801c4cd6d3286
Tetiankaa <tanyakorsun0208@gmail.com>
2024-02-11 16:23:18 +0100

    connect backend to frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {w3cwebsocket as  W3CWebSocket} from "websocket";
+import {useEffect, useState} from "react";
+
+
+const url = 'ws://localhost:8080/chat';
+const client = new W3CWebSocket(url);
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [messageToSend, setMessageToSend] = useState("");
+
+  useEffect(() => {
+
+    client.onmessage = null;
+
+    client.onopen = (e) =>{
+      console.log('connected to chat', e)
+    };
+
+    client.onmessage = (e) =>{
+      setMessages(prevState => [...prevState, e.data]);
+    }
+  }, []);
+
+  function sendMessage(e){
+    e.preventDefault();
+    console.log(messageToSend)
+    client.send(messageToSend);
+    setMessageToSend("");
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <div>
+            <form onSubmit={sendMessage}>
+              <input type="text" placeholder={"Enter message"} value={messageToSend} onChange={(e)=>setMessageToSend(e.target.value)}/>
+              <button>Send</button>
+            </form>
+          </div>
+          {
+            messages.map(msg=><p key={msg}>{msg}</p>)
+          }
+      </>
   );
 }
 
